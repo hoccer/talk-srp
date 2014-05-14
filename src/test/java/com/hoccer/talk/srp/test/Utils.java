f7e801b1f99f3ed4403f9f4610ec7b157d4ff047
Andreas Marr <andreasm@artcom.de>
2014-05-14 11:50:46 +0200

    Fixed a few minor code styling issues.

--- src/test/java/com/hoccer/talk/srp/test/Utils.java
+++ src/test/java/com/hoccer/talk/srp/test/Utils.java
@@ -17,8 +17,9 @@ public class Utils {
     }
 
     public static String toHexString(byte[] buf) {
-        if (buf == null)
+        if (buf == null) {
             return "";
+        }
         StringBuffer result = new StringBuffer(2 * buf.length);
         for (int i = 0; i < buf.length; i++) {
             appendHex(result, buf[i]);
